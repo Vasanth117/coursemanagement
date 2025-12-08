e7a498ef7053288dc92eaf622c78751894ac0632
Vasanth117 <vasanth111907@example.com>
2025-12-08 11:54:08 +0530

    fixing errors

--- backend/src/utils/index.js
+++ backend/src/utils/index.js
@@ -1,7 +1,7 @@
 const Validators = require('./validators');
 const Helpers = require('./helpers');
 const constants = require('./constants');
-const Logger = require('./Logger');
+const Logger = require('./logger');
 const { PERMISSIONS, ROLE_PERMISSIONS, PermissionManager } = require('./permissions');
 const { APIFeatures, createAPIFeatures, FilterHelpers, AggregationHelpers } = require('./apiFeatures');
 
